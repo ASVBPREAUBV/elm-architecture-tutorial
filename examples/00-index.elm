ca7783265c1eea103cd52520a632d563f7ee0a3b
Paul Mayer <paul@paul-mayer.eu>
2017-10-17 19:31:38 +0200

    index init

---
+++ examples/00-index.elm
@@ -0,0 +1,38 @@
+import Html exposing (Html, Attribute, div, fieldset, input, label, text,img)
+import Html.Attributes exposing (name, style,src)
+import Html.Events exposing (onClick)
+import Markdown
+
+
+
+main =
+  Html.beginnerProgram { model = initalModel, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model = Bool
+
+initalModel: Model
+initalModel = False
+
+
+
+-- UPDATE
+
+
+type Msg = ThisIsNotImportant
+
+update : Msg -> Model -> Model
+update msg model = model
+
+
+-- VIEW
+
+
+view :Model -> Html Msg
+view model =
+  div [][text "index"]
+
